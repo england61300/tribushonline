dcf4acc1522571b3881f2eb5ac5dc0bbcea93c99
england61300 <126538708+england61300@users.noreply.github.com>
2024-08-20 10:29:40 -0700

    Create fin-saas-integration.php

---
+++ wp-content/plugins/fin-saas-integration.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: FIN SaaS Integration
+Plugin URI: https://your-website.com/fin-saas-integration
+Description: Integrates the FIN SaaS application into your WordPress website.
+Version: 1.0
+Author: TriBush
+Author URI: https://www.tribushonline.com
+*/
