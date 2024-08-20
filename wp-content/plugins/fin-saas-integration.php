22f62c3e45317c529503ae1da98950866210cf02
england61300 <126538708+england61300@users.noreply.github.com>
2024-08-20 10:30:07 -0700

    Update fin-saas-integration.php

--- wp-content/plugins/fin-saas-integration.php
+++ wp-content/plugins/fin-saas-integration.php
@@ -7,3 +7,8 @@ Version: 1.0
 Author: TriBush
 Author URI: https://www.tribushonline.com
 */
+function fin_saas_enqueue_scripts() {
+    wp_enqueue_script('fin-saas-app', plugin_dir_url(__FILE__) . 'build/static/js/main.js', array(), '1.0', true);
+    wp_enqueue_style('fin-saas-styles', plugin_dir_url(__FILE__) . 'build/static/css/main.css');
+}
+add_action('wp_enqueue_scripts', 'fin_saas_enqueue_scripts');
