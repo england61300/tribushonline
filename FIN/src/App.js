44c151bea443c65c0392281ad8260cbf54cac561
england61300 <126538708+england61300@users.noreply.github.com>
2024-08-25 13:50:13 -0700

    update

--- FIN/src/App.js
+++ FIN/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Financial Calculator SaaS</h1>
+        <h1>Financial Calculator</h1>
       </header>
       <main>
         <div>
